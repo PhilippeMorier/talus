c49185c0e417e84f523b01bb7084343b93fcb944
Philippe Morier <philippe.morier@pix4d.com>
2018-09-06 21:14:38 +0200

    feat(babylon): use babylon ro render basic cube & vertexData

--- src/app/viewport/viewport.component.ts
+++ src/app/viewport/viewport.component.ts
@@ -1,21 +1,14 @@
 import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import {
-  AxesHelper,
-  BufferGeometry,
-  Clock,
-  Float32BufferAttribute,
+  ArcRotateCamera,
+  Engine,
+  HemisphericLight,
   Mesh,
-  MeshBasicMaterial,
-  PerspectiveCamera,
+  MeshBuilder,
   Scene,
-  Vector2,
-  Vector3 as Vec3,
-  WebGLRenderer,
-} from 'three';
-import { getNaiveMesh } from '../mesher/naive-mesher';
-import { OrbitControls } from '../orbit-controls';
-import { sub3, Vector3 } from '../world/vector3';
-import { Voxel, World } from '../world/world';
+  Vector3,
+  VertexData,
+} from 'babylonjs';
 
 @Component({
   selector: 'tls-viewport',
@@ -23,93 +16,71 @@ import { Voxel, World } from '../world/world';
   styleUrls: ['./viewport.component.scss'],
 })
 export class ViewportComponent implements AfterViewInit {
-  private renderer: WebGLRenderer;
+  private engine: Engine;
   private scene: Scene;
-  private camera: PerspectiveCamera;
-  private component: HTMLElement;
-  private controls: OrbitControls;
-  private clock = new Clock(true);
 
   @ViewChild('canvas')
   private canvasRef: ElementRef<HTMLCanvasElement>;
 
-  constructor(private componentRef: ElementRef) {
-    this.component = componentRef.nativeElement as HTMLElement;
-  }
-
   ngAfterViewInit(): void {
-    this.createScene();
-    this.animate();
+    this.engine = new Engine(this.canvasRef.nativeElement, true);
+
+    this.scene = this.createScene();
+
+    this.engine.runRenderLoop(() => {
+      this.scene.render();
+    });
   }
 
-  private createScene(): void {
-    const { x, y } = this.hostDimension;
+  private createScene(): Scene {
+    const scene: Scene = new Scene(this.engine);
 
-    this.camera = new PerspectiveCamera(70, x / y, 0.1, 1000);
-    this.camera.up = new Vec3(0, 0, 1);
-    this.camera.position.z = 30;
+    const camera: ArcRotateCamera = new ArcRotateCamera(
+      'Camera',
+      Math.PI / 2,
+      Math.PI / 2,
+      2,
+      Vector3.Zero(),
+      scene,
+    );
+    camera.attachControl(this.canvasRef.nativeElement, true, false, 2);
+    camera.setPosition(new Vector3(0, 0, 10));
 
-    const world = this.createWorld();
-    const vertices = getNaiveMesh(world, 1, [0, 0, 0], sub3(world.length, [1, 1, 1]));
+    const light1: HemisphericLight = new HemisphericLight('light1', new Vector3(0, 1, 1), scene);
 
-    const geometry = new BufferGeometry();
-    geometry.addAttribute('position', new Float32BufferAttribute(vertices, 3));
-    const material = new MeshBasicMaterial({ color: 0xff0000 });
-    material.wireframe = false;
-    const mesh = new Mesh(geometry, material);
+    const box: Mesh = MeshBuilder.CreateBox('box', { size: 1 }, scene);
 
-    this.scene = new Scene();
-    this.scene.add(new AxesHelper(10));
-    this.scene.add(mesh);
+    const customMesh = new Mesh('custom', scene);
 
-    this.renderer = new WebGLRenderer({ antialias: true, canvas: this.canvasRef.nativeElement });
-    this.renderer.setSize(x, y);
+    const positions = [-5, 2, -3, -7, -2, -3, -3, -2, -3, 5, 2, 3, 7, -2, 3, 3, -2, 3];
+    const indices = [0, 1, 2, 3, 4, 5];
+    const vertexData = new VertexData();
 
-    this.setupCameraControls();
+    vertexData.positions = positions;
+    vertexData.indices = indices;
+    vertexData.applyToMesh(customMesh);
+
+    return scene;
   }
 
-  private setupCameraControls(): void {
-    // this.controls = new FirstPersonControls(this.camera, this.canvasRef.nativeElement);
-    // this.controls.lookSpeed = 0.4;
-    // this.controls.movementSpeed = 10;
-    // this.controls.lookVertical = true;
-    // this.controls.constrainVertical = true;
-    // this.controls.verticalMin = 1.0;
-    // this.controls.verticalMax = 2.0;
-
-    this.controls = new OrbitControls(this.camera, this.scene);
-  }
-
-  private animate = () => {
-    requestAnimationFrame(this.animate);
-
-    this.controls.update(this.clock.getDelta());
-    this.renderer.render(this.scene, this.camera);
-  };
-
-  private get hostDimension(): Vector2 {
-    const rect = this.component.getBoundingClientRect();
-    return new Vector2(rect.width, rect.height);
-  }
-
-  private createWorld(): World {
-    const world = new World([3, 3, 3], [4, 4, 4]);
-    world.setVoxel([0, 0, 0], new Voxel(1, 42));
-    world.setVoxel([1, 1, 1], new Voxel(1, 42));
-    world.setVoxel([2, 2, 2], new Voxel(1, 42));
-    world.setVoxel([3, 3, 3], new Voxel(1, 42));
-    world.setVoxel([4, 4, 4], new Voxel(1, 42));
-
-    const nextRandom = (max: number) => Math.floor(Math.random() * Math.floor(max));
-    for (let i = 0; i < 100; i++) {
-      const position: Vector3 = [
-        nextRandom(3 * 4 - 1),
-        nextRandom(3 * 4 - 1),
-        nextRandom(3 * 4 - 1),
-      ];
-      world.setVoxel(position, new Voxel(1, 42));
-    }
-
-    return world;
-  }
+  // private createWorld(): World {
+  //   const world = new World([3, 3, 3], [4, 4, 4]);
+  //   world.setVoxel([0, 0, 0], new Voxel(1, 42));
+  //   world.setVoxel([1, 1, 1], new Voxel(1, 42));
+  //   world.setVoxel([2, 2, 2], new Voxel(1, 42));
+  //   world.setVoxel([3, 3, 3], new Voxel(1, 42));
+  //   world.setVoxel([4, 4, 4], new Voxel(1, 42));
+  //
+  //   const nextRandom = (max: number) => Math.floor(Math.random() * Math.floor(max));
+  //   for (let i = 0; i < 100; i++) {
+  //     const position: Vector3 = [
+  //       nextRandom(3 * 4 - 1),
+  //       nextRandom(3 * 4 - 1),
+  //       nextRandom(3 * 4 - 1),
+  //     ];
+  //     world.setVoxel(position, new Voxel(1, 42));
+  //   }
+  //
+  //   return world;
+  // }
 }
