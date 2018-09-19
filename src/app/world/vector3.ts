d5a2eb6bcc29287a0b7e587cc83c3ba74ff66dca
Philippe Morier <philippe.morier@pix4d.com>
2018-09-19 21:00:45 +0200

    feat(vector): add XYZ const and power of two check

--- src/app/world/vector3.ts
+++ src/app/world/vector3.ts
@@ -1,5 +1,15 @@
+import { isPowerOfTwo } from './math';
+
+export const X = 0;
+export const Y = 1;
+export const Z = 2;
+
 export type Vector3 = [number, number, number];
 
 export function sub3(a: Vector3, b: Vector3): Vector3 {
   return [a[0] - b[0], a[1] - b[1], a[2] - b[2]];
+}
+
+export function isPowerOfTwo3(a: Vector3): boolean {
+  return isPowerOfTwo(a[X]) && isPowerOfTwo(a[Y]) && isPowerOfTwo(a[Z]);
 }
