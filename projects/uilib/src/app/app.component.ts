e0c2371e78a2102bf3b6502fe0c87aa2208ffde7
Philippe Morier <philippe.morier@pix4d.com>
2019-03-29 17:38:53 +0100

    feat(uilib): add new project for ui components

---
+++ projects/uilib/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tls-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'uilib';
+}
