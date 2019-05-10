65d9ccc5290d429d3f2d66b3e330d02287e5dfa0
Philippe Morier <philippe.morier@pix4d.com>
2019-05-10 17:03:56 +0200

    fix(webVR): no exported member `IDisplayChangedEventArgs`

--- projects/editor/src/app/viewport/viewport.module.ts
+++ projects/editor/src/app/viewport/viewport.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
-import { StoreModule } from '@ngrx/store';
-import { reducer } from './viewport.reducer';
-
 // https://doc.babylonjs.com/features/es6_support
 import '@babylonjs/core/Debug/debugLayer';
+// ERROR in node_modules/@babylonjs/core/Engines/engine.d.ts(8,10):
+// error TS2305: Module '"./engine"' has no exported member 'IDisplayChangedEventArgs'.
+import '@babylonjs/core/Engines/Extensions/engine.webVR';
 import '@babylonjs/inspector';
+import { StoreModule } from '@ngrx/store';
+import { reducer } from './viewport.reducer';
 
 @NgModule({
   imports: [StoreModule.forFeature('viewport', reducer)],
