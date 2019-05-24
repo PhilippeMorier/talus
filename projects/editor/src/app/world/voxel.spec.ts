da8a1e78c4c2d4ec05a570589a5853e65e4eb1ab
Philippe Morier <philippe.morier@pix4d.com>
2019-05-24 13:43:46 +0200

    refactor: uppercase `Voxel`

--- projects/editor/src/app/world/voxel.spec.ts
+++ projects/editor/src/app/world/voxel.spec.ts
@@ -1,6 +1,6 @@
 import { toVoxel, toVoxelValue, Voxel } from './voxel';
 
-describe('voxel', () => {
+describe('Voxel', () => {
   it('should convert voxel', () => {
     const expectedVoxel = new Voxel(32767, 65535);
     const voxelValue = toVoxelValue(expectedVoxel);
