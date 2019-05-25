c71b145b830142c5f3f688b61c74db65d9cca46d
Philippe Morier <philippe.morier@pix4d.com>
2019-05-25 17:14:51 +0200

    refactor: add `viewport` state to main state

--- projects/editor/src/app/reducers/index.ts
+++ projects/editor/src/app/reducers/index.ts
@@ -6,9 +6,14 @@ import {
   MetaReducer,
 } from '@ngrx/store';
 import { environment } from '../../environments/environment';
+import * as fromViewport from '../editor-shell/viewport/viewport.reducer';
 
-export interface State {}
+export interface State {
+  viewport: fromViewport.State;
+}
 
-export const reducers: ActionReducerMap<State> = {};
+export const reducers: ActionReducerMap<State> = {
+  viewport: fromViewport.reducer,
+};
 
 export const metaReducers: Array<MetaReducer<State>> = !environment.production ? [] : [];
