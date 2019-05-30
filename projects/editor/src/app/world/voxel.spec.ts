08b1a56b7ac2709d3b9a9715f94da5622365c129
Circle CI <morier.dev@outlook.com>
2019-05-30 14:11:20 +0000

    [skip ci] Updates

--- projects/editor/src/app/world/voxel.spec.ts
+++
@@ -1,9 +0,0 @@
-import { toVoxel, toVoxelValue, Voxel } from './voxel';
-
-describe('Voxel', () => {
-  it('should convert voxel', () => {
-    const expectedVoxel = new Voxel(32767, 65535);
-    const voxelValue = toVoxelValue(expectedVoxel);
-    expect(toVoxel(voxelValue)).toEqual(expectedVoxel);
-  });
-});
