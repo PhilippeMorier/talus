c3a599bc8f99cd690777eb5dac7c16621c677c62
Philippe Morier <philippe.morier@pix4d.com>
2019-05-31 15:42:37 +0200

    refactor: import `@babylonjs/materials`ˆ

--- projects/editor/src/app/editor-shell/viewport/viewport.module.ts
+++ projects/editor/src/app/editor-shell/viewport/viewport.module.ts
@@ -4,11 +4,12 @@ import '@babylonjs/core/Debug/debugLayer';
 // ERROR in node_modules/@babylonjs/core/Engines/engine.d.ts(8,10):
 // error TS2305: Module '"./engine"' has no exported member 'IDisplayChangedEventArgs'.
 import '@babylonjs/core/Engines/Extensions/engine.webVR';
-// Uncomment to make `this.scene.debugLayer.show()` work
-// import '@babylonjs/inspector';
+import '@babylonjs/materials';
 import { StoreModule } from '@ngrx/store';
 import { ViewportComponent } from './viewport.component';
 import { reducer } from './viewport.reducer';
+// Uncomment to make `this.scene.debugLayer.show()` work
+// import '@babylonjs/inspector';
 
 @NgModule({
   declarations: [ViewportComponent],
