fb9da2698a596e524e81fe5eb8405d222c8d915c
Philippe Morier <philippe.morier@pix4d.com>
2019-07-05 13:32:34 +0200

    feat: add sidenav with material

--- apps/frontend/src/app/app.module.ts
+++ apps/frontend/src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SidenavShellModule } from '@talus/ui';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     RouterModule.forRoot([], { initialNavigation: 'enabled' }),
 
     SidenavShellModule
--- libs/ui/src/lib/sidenav-shell/sidenav-shell.module.ts
+++ libs/ui/src/lib/sidenav-shell/sidenav-shell.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidenavShellComponent } from './sidenav-shell.component';
+import { MatButtonModule, MatIconModule, MatSidenavModule } from '@angular/material';
 
 @NgModule({
   declarations: [SidenavShellComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, MatButtonModule, MatIconModule, MatSidenavModule],
   exports: [SidenavShellComponent]
 })
 export class SidenavShellModule {}
