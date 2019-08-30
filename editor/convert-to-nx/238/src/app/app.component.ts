1dac0f51af3c65f3dfb5af025aba7bbe7cc1d185
Circle CI <morier.dev@outlook.com>
2019-08-30 13:04:50 +0000

    [skip ci] Deploy: editor (#238)

---
+++ editor/convert-to-nx/238/src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'fe-root',
+  template: `
+    <ui-sidenav-shell>
+      <ui-sidenav-shell-left>
+        Left
+      </ui-sidenav-shell-left>
+
+      <ui-sidenav-shell-right>
+        Right
+      </ui-sidenav-shell-right>
+
+      <ui-sidenav-shell-content>
+        <ui-scene-viewer></ui-scene-viewer>
+      </ui-sidenav-shell-content>
+    </ui-sidenav-shell>
+  `,
+})
+export class AppComponent {
+  title = 'frontend';
+}
