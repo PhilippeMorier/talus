045f51284a52dc55b7b5e004c2d2bd18cfe2c235
PhilippeMorier <morier.dev@outlook.com>
2019-10-17 15:27:18 +0200

    test: add benchmark

--- libs/math/src/lib/math.benchmark.ts
+++ libs/math/src/lib/math.benchmark.ts
@@ -4,32 +4,54 @@
 
 import { Suite } from 'benchmark';
 
-const suite = new Suite();
+const suites: Suite[] = [];
 
-describe('Integer division', () => {
-  it('Integer division', () => {
-    console.log(suite);
+function suite(name: string, benchmarks: () => void): void {
+  it(name, () => {
+    const newSuite = new Suite(name);
+    suites.push(newSuite);
 
-    // add tests
-    suite
-      .add('RegExp#test', () => {
-        /o/.test('Hello World!');
-      })
-      .add('String#indexOf', () => {
-        'Hello World!'.indexOf('o') > -1;
-      })
-      .add('String#match', () => {
-        !!'Hello World!'.match(/o/);
-      })
-      // add listeners
-      .on('cycle', event => {
-        console.log(event);
-        console.log('@@@ cycle ' + String(event.target));
-      })
-      .on('complete', () => {
-        console.log('Fastest is ' + suite.filter('fastest').map(benchmark => benchmark.name));
-      })
-      // run async
-      .run();
+    let cycleLog = '';
+    newSuite.on('cycle', event => {
+      cycleLog += `  - ${event.target}\n`;
+    });
+    newSuite.on('complete', () => {
+      const fastestBenchmarkName = newSuite.filter('fastest').map(bm => bm.name);
+      console.log(`Fastest is ${fastestBenchmarkName}\n${cycleLog}`);
+    });
+
+    benchmarks();
+
+    newSuite.run();
+  });
+}
+
+function benchmark(name: string, fn: () => void): void {
+  suites[suites.length - 1].add(name, fn);
+}
+
+suite('Integer division', () => {
+  benchmark('Math.floor()', () => {
+    Math.floor(10 / 2);
+    Math.floor(11 / 3);
+    Math.floor(12 / 5);
+  });
+
+  benchmark('a/b >> 0', () => {
+    (10 / 2) >> 0;
+    (11 / 3) >> 0;
+    (12 / 5) >> 0;
+  });
+
+  benchmark('~~(a/b)', () => {
+    ~~(10 / 2);
+    ~~(11 / 3);
+    ~~(12 / 5);
+  });
+
+  benchmark('a/b | 0', () => {
+    (10 / 2) | 0;
+    (11 / 3) | 0;
+    (12 / 5) | 0;
   });
 });
