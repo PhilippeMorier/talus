e13bdcf9f7b8e8c20dda9627bdf5e1af05143616
PhilippeMorier <morier.dev@outlook.com>
2019-10-17 14:01:46 +0200

    test: simple naive benchmark test

--- libs/math/src/lib/math.benchmark.ts
+++ libs/math/src/lib/math.benchmark.ts
@@ -1,7 +1,35 @@
-// const benchmark = require('benchmark');
-//
-// describe('Integer division', () => {
-//   fit('Integer division', done => {
-//     console.log(benchmark);
-//   });
-// });
+/**
+ * @jest-environment node
+ */
+
+import { Suite } from 'benchmark';
+
+const suite = new Suite();
+
+describe('Integer division', () => {
+  it('Integer division', () => {
+    console.log(suite);
+
+    // add tests
+    suite
+      .add('RegExp#test', () => {
+        /o/.test('Hello World!');
+      })
+      .add('String#indexOf', () => {
+        'Hello World!'.indexOf('o') > -1;
+      })
+      .add('String#match', () => {
+        !!'Hello World!'.match(/o/);
+      })
+      // add listeners
+      .on('cycle', event => {
+        console.log(event);
+        console.log('@@@ cycle ' + String(event.target));
+      })
+      .on('complete', () => {
+        console.log('Fastest is ' + suite.filter('fastest').map(benchmark => benchmark.name));
+      })
+      // run async
+      .run();
+  });
+});
