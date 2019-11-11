76d4f7de1f8b897fc1d063abbd6d1b0e67734061
PhilippeMorier <morier.dev@outlook.com>
2019-11-11 15:41:27 +0100

    feat(libs/vdb): add skeleton for new lib

---
+++ libs/vdb/jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: 'vdb',
+  preset: '../../jest.config.js',
+  transform: {
+    '^.+\\.[tj]sx?$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'html'],
+  coverageDirectory: '../../coverage/libs/vdb',
+};
---
+++ libs/vdb/src/lib/math/coord.ts
@@ -0,0 +1,5 @@
+export const X = 0;
+export const Y = 1;
+export const Z = 2;
+
+export type Coord = [number, number, number];
