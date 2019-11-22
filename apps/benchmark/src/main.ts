7860d8b9a6732aab709a9489f9dc1b40a1d25c81
PhilippeMorier <morier.dev@outlook.com>
2019-11-22 17:30:02 +0100

    refactor(apps/benchmark): print sorted Hz graph

--- apps/benchmark/src/main.ts
+++ apps/benchmark/src/main.ts
@@ -6,17 +6,7 @@ export function suite(name: string, benchmarksFn: () => void): void {
   const newSuite = new Suite(name);
   suites.push(newSuite);
 
-  newSuite.on('complete', event => {
-    const fastestBenchmarkName = newSuite.filter('fastest').map(bm => bm.name);
-    const conjugatedVerbBe = fastestBenchmarkName.length > 1 ? 'are' : 'is';
-
-    console.log(name);
-    console.log(`  Fastest ${conjugatedVerbBe} [${fastestBenchmarkName.join(', ')}]`);
-
-    const currentSuite = event.currentTarget;
-    const benchmarks = Array.from({ length: currentSuite.length }, (x, i) => currentSuite[i]);
-    benchmarks.forEach(bm => console.log(`  - ${bm}`));
-  });
+  newSuite.on('complete', event => logSummary(event.currentTarget));
 
   benchmarksFn();
 
@@ -25,6 +15,31 @@ export function suite(name: string, benchmarksFn: () => void): void {
 
 export function benchmark(name: string, fn: () => void): void {
   suites[suites.length - 1].add(name, fn);
+}
+
+function logSummary(suiteToLog: any): void {
+  const fastestBenchmarkName = suiteToLog.filter('fastest').map(bm => bm.name);
+  const conjugatedVerbBe = fastestBenchmarkName.length > 1 ? 'are' : 'is';
+  console.log(suiteToLog.name);
+  console.log(`  Fastest ${conjugatedVerbBe} [${fastestBenchmarkName.join(', ')}]`);
+
+  const benchmarks = Array.from({ length: suiteToLog.length }, (x, i) => suiteToLog[i]);
+  const descSortedBenchmarks = benchmarks.sort((a, b) => b.hz - a.hz);
+  const highestHz = descSortedBenchmarks[0].hz;
+
+  descSortedBenchmarks.forEach(bm => {
+    const barPercentage = (100 / highestHz) * bm.hz;
+    const charsPerOnePercentage = 2;
+
+    const filledBarLength = barPercentage / charsPerOnePercentage;
+    const emptyBarLength = (100 - barPercentage) / charsPerOnePercentage;
+
+    const createBar = (length, char) => Array.from({ length }, () => char).join('');
+    const filledBar = `${createBar(Math.floor(filledBarLength), '-')}`;
+    const emptyBar = createBar(Math.ceil(emptyBarLength), ' ');
+
+    console.log(`  |${filledBar}${emptyBar}| ${bm}`);
+  });
 }
 
 declare const require: any;
