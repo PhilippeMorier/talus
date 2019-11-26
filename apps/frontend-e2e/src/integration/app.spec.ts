2fde39fcd0c2dae8783f898c70d9aaa9e29642e9
PhilippeMorier <morier.dev@outlook.com>
2019-11-26 11:45:19 +0100

    build(@cypress): generate & store JUnit results

--- apps/frontend-e2e/src/integration/app.spec.ts
+++ apps/frontend-e2e/src/integration/app.spec.ts
@@ -3,7 +3,7 @@ import { getSidenavLeft } from '../support/app.po';
 describe('frontend', () => {
   beforeEach(() => cy.visit('/'));
 
-  it('should display welcome message', () => {
+  it('should display left side navigation', () => {
     getSidenavLeft().contains('Left');
   });
 });
--- apps/frontend-e2e/src/support/app.po.ts
+++ apps/frontend-e2e/src/support/app.po.ts
@@ -1,1 +1,2 @@
 export const getSidenavLeft = () => cy.get('ui-sidenav-shell-left');
+export const getSidenavRight = () => cy.get('ui-sidenav-shell-right');
