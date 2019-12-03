3eb3b3b2b1e83507e81eba65f0f48f90eedb5d86
PhilippeMorier <morier.dev@outlook.com>
2019-12-03 18:38:21 +0100

    test(apps/benchmark): benchmark `setValue()`

--- apps/benchmark/src/app/vdb/grid.benchmark.ts
+++ apps/benchmark/src/app/vdb/grid.benchmark.ts
@@ -1,19 +1,54 @@
-import { Grid, InternalNode2 } from '@talus/vdb';
+import { Grid } from '@talus/vdb';
 import { benchmark, suite } from '../../main';
 
 suite('[Grid] getValue()', () => {
   const grid = new Grid(-1);
   const accessor = grid.getAccessor();
+  const i = 42;
 
   benchmark('tree', () => {
-    for (let z = 0; z < InternalNode2.DIM; z++) {
-      grid.tree.getValue([0, 0, z]);
+    for (let x = 0; x < i; x++) {
+      for (let y = 0; y < i; y++) {
+        for (let z = 0; z < i; z++) {
+          grid.tree.getValue([x, y, z]);
+        }
+      }
     }
   });
 
   benchmark('accessor', () => {
-    for (let z = 0; z < InternalNode2.DIM; z++) {
-      accessor.getValue([0, 0, z]);
+    for (let x = 0; x < i; x++) {
+      for (let y = 0; y < i; y++) {
+        for (let z = 0; z < i; z++) {
+          accessor.getValue([x, y, z]);
+        }
+      }
+    }
+  });
+});
+
+suite('[Grid] setValue()', () => {
+  const grid = new Grid(-1);
+  const accessor = grid.getAccessor();
+  const i = 42;
+
+  benchmark('tree', () => {
+    for (let x = 0; x < i; x++) {
+      for (let y = 0; y < i; y++) {
+        for (let z = 0; z < i; z++) {
+          grid.tree.setValueOn([x, y, z], i);
+        }
+      }
+    }
+  });
+
+  benchmark('accessor', () => {
+    for (let x = 0; x < i; x++) {
+      for (let y = 0; y < i; y++) {
+        for (let z = 0; z < i; z++) {
+          accessor.setValue([x, y, z], i);
+        }
+      }
     }
   });
 });
