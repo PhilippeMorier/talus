4d713bdaa62721398289676ba47b7623fe339222
PhilippeMorier <morier.dev@outlook.com>
2019-12-03 13:44:57 +0100

    test(libs/vdb): remove empty line and extract variable

--- libs/vdb/src/lib/tree/value-accessor.spec.ts
+++ libs/vdb/src/lib/tree/value-accessor.spec.ts
@@ -24,12 +24,12 @@ describe('ValueAccessor', () => {
   describe('setValue()', () => {
     it('should cache coordinate', () => {
       const tree = new Tree(0);
-
       const accessor = new ValueAccessor3(tree);
 
       accessor.setValue([0, 0, 0], 1496);
+      const value = accessor.getValue([0, 0, 0]);
 
-      expect(tree.getValue([0, 0, 0])).toEqual(1496);
+      expect(value).toEqual(1496);
       expect(accessor.isCached([0, 0, 0])).toBeTruthy();
       expect(accessor.isCached([0, 0, 8])).toBeTruthy();
       expect(accessor.isCached([0, 8, 0])).toBeTruthy();
