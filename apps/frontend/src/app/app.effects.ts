4ad0a040d87177bc2db08f0b1069450cce6ff3d2
PhilippeMorier <morier.dev@outlook.com>
2019-12-12 15:03:37 +0100

    test(frontend): on/offline effects

--- apps/frontend/src/app/app.effects.ts
+++ apps/frontend/src/app/app.effects.ts
@@ -1,13 +1,11 @@
 import { Injectable } from '@angular/core';
-import { Actions, createEffect } from '@ngrx/effects';
+import { createEffect } from '@ngrx/effects';
 import { fromEvent, merge, of } from 'rxjs';
 import { map, mapTo } from 'rxjs/operators';
 import { wentOffline, wentOnline } from './app.actions';
 
 @Injectable()
 export class AppEffects {
-  constructor(private actions$: Actions) {}
-
   // @source: https://indepth.dev/start-using-ngrx-effects-for-this/#1externalsources
   onlineStateChange$ = createEffect(() => {
     return merge(
