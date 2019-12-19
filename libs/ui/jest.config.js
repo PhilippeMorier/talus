596f386946c7d3a969dc19105a352a5d654dd1e2
PhilippeMorier <morier.dev@outlook.com>
2019-12-19 12:40:26 +0100

    test(frontend): set `globals`>`tsConfig`
    - to run in Jest tests within WebStorm with SCSS imports in component

--- libs/ui/jest.config.js
+++ libs/ui/jest.config.js
@@ -19,4 +19,23 @@ module.exports = {
     'jest-preset-angular/AngularSnapshotSerializer.js',
     'jest-preset-angular/HTMLCommentSerializer.js',
   ],
+  // https://github.com/nrwl/nx/issues/1439#issuecomment-561268656
+  // When using `Run test` directly in WebStorm, change the used config to
+  // this file i.e. `./ui/jest.config.js` and not `<rootDir>/jest.config.js`.
+  // Otherwise, following error might occur:
+  // - Cannot find module '@talus/ui'
+  // - Zone is needed for the async() test helper but could not be found.
+  setupFilesAfterEnv: ['./src/test-setup.ts'],
+
+  // https://github.com/thymikee/jest-preset-angular/issues/293#issuecomment-513544717
+  // When using `Run test` directly in WebStorm, the scss couldn't be loaded.
+  // - Error: connect ECONNREFUSED 127.0.0.1:80
+  // - Error: Uncaught (in promise): Failed to load *.component.scss
+  globals: {
+    'ts-jest': {
+      tsConfig: '<rootDir>/tsconfig.spec.json',
+      // stringifyContentPathRegex: '\\.html$',
+      // astTransformers: [require.resolve('jest-preset-angular/InlineHtmlStripStylesTransformer')],
+    },
+  },
 };
