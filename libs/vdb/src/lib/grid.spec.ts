a387799df0ae23d63615a3d0c3d62b840830e7e4
PhilippeMorier <morier.dev@outlook.com>
2020-01-16 14:17:15 +0100

    test(vdb): check grid

---
+++ libs/vdb/src/lib/grid.spec.ts
@@ -0,0 +1,26 @@
+import { Grid } from '@talus/vdb';
+
+describe('Grid', () => {
+  it('should get background', () => {
+    const grid = new Grid(-1);
+
+    expect(grid.background).toEqual(-1);
+  });
+
+  it('should iterate over each activated voxel', () => {
+    const grid = new Grid(-1);
+    const accessor = grid.getAccessor();
+
+    accessor.setValueOn([2, 1, 0], 42);
+    accessor.setValueOn([845, 64, 242], 42);
+    accessor.setValueOn([1000, 200000, 4000], 42);
+
+    let counter = 0;
+    for (const voxel of grid.beginVoxelOn()) {
+      counter++;
+      expect(voxel.value).toEqual(42);
+    }
+
+    expect(counter).toEqual(3);
+  });
+});
