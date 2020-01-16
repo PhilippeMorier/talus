d286d109d67f4d63fe05cf78fac20c488d5dcc5e
PhilippeMorier <morier.dev@outlook.com>
2020-01-16 09:53:38 +0100

    test(ui): should emit on tool select

--- libs/ui/src/lib/toolbar/toolbar.component.spec.ts
+++ libs/ui/src/lib/toolbar/toolbar.component.spec.ts
@@ -1,10 +1,24 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { UiToolbarComponent } from './toolbar.component';
+import { By } from '@angular/platform-browser';
+import { UiToolbarComponent, UiToolbarToolConfig } from './toolbar.component';
 import { UiToolbarModule } from './toolbar.module';
 
 describe('ToolbarComponent', () => {
   let component: UiToolbarComponent;
   let fixture: ComponentFixture<UiToolbarComponent>;
+
+  const tools: UiToolbarToolConfig<string>[] = [
+    {
+      icon: 'add_circle_outline',
+      tooltip: 'Add',
+      value: 'Add test value',
+    },
+    {
+      icon: 'remove_circle_outline',
+      tooltip: 'Remove',
+      value: 'Remove test value',
+    },
+  ];
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -15,10 +29,27 @@ describe('ToolbarComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(UiToolbarComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    // fixture.detectChanges();
+  });
+
+  beforeEach(() => {
+    spyOn(component.toolChange, 'emit');
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should emit tool change', () => {
+    component.tools = tools;
+    fixture.detectChanges();
+
+    const buttonToggles = fixture.debugElement.queryAll(By.css('button.mat-button-toggle-button'));
+    expect(buttonToggles.length).toEqual(tools.length);
+
+    buttonToggles[0].nativeElement.click();
+    fixture.detectChanges();
+
+    expect(component.toolChange.emit).toHaveBeenCalled();
   });
 });
