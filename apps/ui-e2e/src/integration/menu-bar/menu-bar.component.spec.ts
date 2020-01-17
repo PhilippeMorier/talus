dbee49abbcae7eb96af498a865785f82a8676f19
PhilippeMorier <morier.dev@outlook.com>
2020-01-17 11:59:27 +0100

    test(ui-e2e): open/close `menu-bar`

--- apps/ui-e2e/src/integration/menu-bar/menu-bar.component.spec.ts
+++ apps/ui-e2e/src/integration/menu-bar/menu-bar.component.spec.ts
@@ -1,11 +1,40 @@
+import { UiMenuBarConfig } from '@talus/ui';
+
+const menuConfig: UiMenuBarConfig<string> = {
+  menus: [
+    {
+      label: 'Edit',
+      menuItems: [
+        {
+          icon: 'undo',
+          label: 'Undo',
+          value: 'undo()',
+        },
+        {
+          icon: 'redo',
+          label: 'Redo',
+          value: 'redo()',
+        },
+      ],
+    },
+  ],
+};
+
 describe('ui', () => {
   beforeEach(() =>
     cy.visit(
-      '/iframe.html?id=uimenubarcomponent--primary&knob-menuConfig={"menus":[{"label":"Edit","menuItems":[{"icon":"undo","label":"Undo","value":"Test%20undo%20value"},{"icon":"redo","label":"Redo","value":"Test%20redo%20value"}]}]}',
+      `/iframe.html?id=uimenubarcomponent--primary&knob-menuConfig=${JSON.stringify(menuConfig)}`,
     ),
   );
 
-  it('should render', () => {
+  it('should render all menus/menu-items & and open/close menu', () => {
     cy.get('ui-menu-bar').should('exist');
+
+    cy.get('.mat-menu-trigger').click();
+    cy.get('.mat-menu-content').should('have.length', menuConfig.menus.length);
+    cy.get('.mat-menu-item').should('have.length', menuConfig.menus[0].menuItems.length);
+
+    cy.get('.cdk-overlay-backdrop').click();
+    cy.get('.mat-menu-content').should('not.exist');
   });
 });
