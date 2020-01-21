ef0f7add4e93328b295f775901b9edfb250be9a3
PhilippeMorier <morier.dev@outlook.com>
2020-01-21 14:55:46 +0100

    feat(frontend): open (empty) color-dialog

--- apps/frontend/src/app/options-panel/options-panel.component.ts
+++ apps/frontend/src/app/options-panel/options-panel.component.ts
@@ -1,14 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { UiColorDialogService } from '@talus/ui';
 
 @Component({
   selector: 'fe-options-panel',
   template: `
-    <button mat-icon-button>
+    <button mat-icon-button (click)="onClick()">
       <mat-icon>color_lens</mat-icon>
       <mat-icon id="more-caret-icon">signal_cellular_4_bar</mat-icon>
       <mat-icon id="color-icon">fiber_manual_record</mat-icon>
     </button>
   `,
   styleUrls: ['./options-panel.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class OptionsPanelComponent {}
+export class OptionsPanelComponent {
+  constructor(public colorDialogService: UiColorDialogService) {}
+
+  onClick(): void {
+    this.colorDialogService.open([]);
+  }
+}
--- apps/frontend/src/app/options-panel/options-panel.module.ts
+++ apps/frontend/src/app/options-panel/options-panel.module.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MatButtonModule, MatIconModule } from '@angular/material';
+import { UiColorDialogModule } from '@talus/ui';
 import { OptionsPanelComponent } from './options-panel.component';
 
 @NgModule({
   declarations: [OptionsPanelComponent],
-  imports: [CommonModule, MatButtonModule, MatIconModule],
+  imports: [CommonModule, MatButtonModule, MatIconModule, UiColorDialogModule],
   exports: [OptionsPanelComponent],
 })
 export class OptionsPanelModule {}
