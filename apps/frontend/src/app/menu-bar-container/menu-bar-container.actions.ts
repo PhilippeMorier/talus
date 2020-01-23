8bc30ef7cb9996e51e4a4697eabc27a543377bd6
PhilippeMorier <morier.dev@outlook.com>
2020-01-23 15:51:26 +0100

    feat(frontend): support dark & light theme

--- apps/frontend/src/app/menu-bar-container/menu-bar-container.actions.ts
+++ apps/frontend/src/app/menu-bar-container/menu-bar-container.actions.ts
@@ -4,3 +4,6 @@ const actionTypePrefix = `[menuBarContainer]`;
 
 export const undo = createAction(`${actionTypePrefix} Undo`);
 export const redo = createAction(`${actionTypePrefix} Redo`);
+
+export const setDarkTheme = createAction(`${actionTypePrefix} Set dark theme`);
+export const setLightTheme = createAction(`${actionTypePrefix} Set light theme`);
--- apps/frontend/src/app/menu-bar-container/menu-bar-container.module.ts
+++ apps/frontend/src/app/menu-bar-container/menu-bar-container.module.ts
@@ -1,10 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { UiMenuBarModule } from '@talus/ui';
 import { MenuBarContainerComponent } from './menu-bar-container.component';
 
 @NgModule({
   declarations: [MenuBarContainerComponent],
-  imports: [UiMenuBarModule],
+  imports: [CommonModule, UiMenuBarModule],
   exports: [MenuBarContainerComponent],
 })
 export class MenuBarContainerModule {}
