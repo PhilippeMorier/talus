fc9e05cd04bca97493dd5f4b21446a8095c6f770
PhilippeMorier <morier.dev@outlook.com>
2020-02-21 15:21:19 +0100

    test(ui): fix providers for session-dialog spec

--- libs/ui/src/lib/session-dialog/session-dialog.component.spec.ts
+++ libs/ui/src/lib/session-dialog/session-dialog.component.spec.ts
@@ -1,20 +1,27 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { UiSessionDialogModule } from '@talus/ui';
+import { EMPTY } from 'rxjs';
 
-import { SessionDialogComponent } from './session-dialog.component';
+import { UiSessionDialogComponent } from './session-dialog.component';
 
 describe('SessionDialogComponent', () => {
-  let component: SessionDialogComponent;
-  let fixture: ComponentFixture<SessionDialogComponent>;
+  let component: UiSessionDialogComponent;
+  let fixture: ComponentFixture<UiSessionDialogComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SessionDialogComponent ]
-    })
-    .compileComponents();
+      imports: [UiSessionDialogModule],
+      providers: [
+        // https://github.com/angular/components/issues/8419#issuecomment-361972699
+        { provide: MatDialogRef, useValue: { close: () => {} } },
+        { provide: MAT_DIALOG_DATA, useValue: EMPTY },
+      ],
+    }).compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(SessionDialogComponent);
+    fixture = TestBed.createComponent(UiSessionDialogComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
