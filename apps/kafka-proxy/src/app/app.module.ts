4c4908dec581dda0518505e60e42b970f869fd04
PhilippeMorier <morier.dev@outlook.com>
2020-02-26 11:21:51 +0100

    refactor(kafka-proxy): remove scaffolded files

--- apps/kafka-proxy/src/app/app.module.ts
+++ apps/kafka-proxy/src/app/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { KafkaModule } from './kafka/kafka.module';
 
 @Module({
   imports: [KafkaModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
