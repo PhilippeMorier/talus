18b83aa7ea7188bdf89e938c8b035ef51d917ccf
PhilippeMorier <morier.dev@outlook.com>
2020-03-17 17:09:47 +0100

    style(frontend): fix order

--- apps/frontend/src/app/scene-viewer-container/load-file-container/load-file-container.module.ts
+++ apps/frontend/src/app/scene-viewer-container/load-file-container/load-file-container.module.ts
@@ -1,8 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { UiProgressSpinnerModule } from '@talus/ui';
-import { LoadFileService } from './load-file.service';
 import { LoadFileContainerComponent } from './load-file-container.component';
+import { LoadFileService } from './load-file.service';
 
 @NgModule({
   declarations: [LoadFileContainerComponent],
