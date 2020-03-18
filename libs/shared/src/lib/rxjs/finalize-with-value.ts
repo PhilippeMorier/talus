c28f7d86346f17fc2683c21de84d4b52e9e1d407
Philippe Morier <PhilippeMorier@users.noreply.github.com>
2020-03-18 10:35:08 +0100

    build(dep): update outdated dependencies (#43)
    * build(dep): bump `@nestjs` to `7.0.3`
    
    * build(dep): bump `@nrwl` to `9.1.2`
    
    * build(dep): bump div dep
    
    * build(dep): bump @angular cdk & material
    
    * build(dep): bump `cypress`, `zone.js` & `jest-preset-angular`
    
    * build(dep): revert to `eslint/parser` `2.23.0`:
    
    - https://github.com/typescript-eslint/typescript-eslint/issues/1746
    
    * refactor(frontend): add comment about not dispatching action with `File`

--- libs/shared/src/lib/rxjs/finalize-with-value.ts
+++ libs/shared/src/lib/rxjs/finalize-with-value.ts
@@ -1,11 +1,11 @@
-// https://github.com/ReactiveX/rxjs/issues/4803#issuecomment-496711335
 import { defer, Observable, ObservedValueOf } from 'rxjs';
 import { finalize, tap } from 'rxjs/operators';
 
+// https://github.com/ReactiveX/rxjs/issues/4803#issuecomment-496711335
 export function finalizeWithValue<T>(
   callback: (value: T) => void,
 ): (source: Observable<T>) => Observable<ObservedValueOf<Observable<T>>> {
-  return (source: Observable<T>) =>
+  return (source: Observable<T>): Observable<T> =>
     defer(() => {
       let lastValue: T;
       return source.pipe(
