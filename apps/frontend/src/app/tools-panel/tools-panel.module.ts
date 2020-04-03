16eb331994515905455ff6f4f60f17e353047219
PhilippeMorier <morier.dev@outlook.com>
2020-04-03 11:39:30 +0200

    test(frontend): provide mock store & actions

--- apps/frontend/src/app/tools-panel/tools-panel.module.ts
+++ apps/frontend/src/app/tools-panel/tools-panel.module.ts
@@ -2,12 +2,14 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { EffectsModule } from '@ngrx/effects';
 import { UiToolbarModule } from '@talus/ui';
+import { GridService } from '../scene-viewer-container/grid.service';
 import { ToolsPanelComponent } from './tools-panel.component';
 import { ToolsPanelEffects } from './tools-panel.effects';
 
 @NgModule({
   declarations: [ToolsPanelComponent],
   imports: [CommonModule, EffectsModule.forFeature([ToolsPanelEffects]), UiToolbarModule],
+  providers: [GridService],
   exports: [ToolsPanelComponent],
 })
 export class ToolsPanelModule {}
