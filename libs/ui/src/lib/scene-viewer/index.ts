d58f637dd3513a1c107095c3e3d14c68fff53cd2
PhilippeMorier <morier.dev@outlook.com>
2020-04-03 08:54:31 +0200

    refactor(lib): export all

--- libs/ui/src/lib/scene-viewer/index.ts
+++ libs/ui/src/lib/scene-viewer/index.ts
@@ -2,4 +2,4 @@ export * from './scene-viewer.component';
 export * from './scene-viewer.module';
 export * from './scene-viewer.module.testing';
 export { UiPointerButton } from './pointer-button';
-export { UiPointerPickInfo, UiSceneViewerService } from './scene-viewer.service';
+export * from './scene-viewer.service';
