6f96d9da3e0080d2d77a0139fb081cf0dbc62d6b
PhilippeMorier <morier.dev@outlook.com>
2020-04-14 13:50:44 +0200

    doc: add comment for new `testMatch` pattern

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,6 @@
 module.exports = {
+  // With new jest version 25, `No tests found, exiting with code 0` occurred with old pattern.
+  // Therefore, use default from https://jestjs.io/docs/en/configuration#testmatch-arraystring
   testMatch: ['**/+(*.)+(spec|test).[jt]s?(x)'],
   transform: {
     '^.+\\.(ts|js|html)$': 'ts-jest',
